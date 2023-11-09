34d62ddeb2bab1771d36d8f7bd905bea5765c701
Hwangseongyoung <dfghukl1333@naver.com>
2023-11-09 21:38:27 +0900

    + visible

--- js/anchor.js
+++ js/anchor.js
@@ -5,7 +5,9 @@ document.addEventListener('scroll', () => {
     
     if (myScroll < 1800) {
         document.querySelector('.anchor').style.opacity = '0';
+        document.querySelector('.anchor').style.visibility = "hidden";
     } else {
         document.querySelector('.anchor').style.opacity = '1';
+        document.querySelector('.anchor').style.visibility = "visible";
     }
 })
